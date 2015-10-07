a637072c2201159f2b7bd3c2c1e69bd62abd3749
Anthony Gore <anthony@alliedpublishers.com.au>
2015-10-07 12:19:05 +1100

    Fixing bug with css import

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var path = require('path');
 
 module.exports = {
     name: 'ember-cli-auto-complete',
-    include: function(app) {
+    include: function include(app) {
 	    if (app.app) {
 	        app = app.app;
 	    }
 	    this.app = app;
-	    app.import(path.join('vendor', 'auto-complete.css'));
+	    //app.import(path.join('vendor', 'auto-complete.css'));
+	    app.import('vendor/auto-complete.css');
 	}
 };
